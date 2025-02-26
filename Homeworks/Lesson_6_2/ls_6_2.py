d4a3ecdca2c11cae307209fcb8956ceaface1309
neocom21 <neocom21@gmail.com>
2025-02-26 10:19:34 +0200

    lesson 6_2

---
+++ Homeworks/Lesson_6_2/ls_6_2.py
@@ -0,0 +1,12 @@
+
+#print("\nДовідково ascii_letters: "+ my_ascii+'\n')
+
+#my_variable=input("Ведіть через дефіс дві літери (наприклад: s-H): ")
+
+my_variable = 8640000
+
+kol_day, kol_god= divmod(my_variable, 24*60*60)
+
+print(kol_day, kol_god)
+
+
