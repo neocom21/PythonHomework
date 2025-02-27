dde59a784ea81d0b39238b130fa5c5a021f6eaed
neocom21 <neocom21@gmail.com>
2025-02-27 08:52:12 +0200

    lesson_5_1_2

---
+++ Homeworks/Lesson_5_1/ls_5_1_2.py
@@ -0,0 +1,50 @@
+'''
+_ => True
+__ => False
+___ => False
+x => True
+get_value => True
+get value => False
+get!value => False
+some_super_puper_value => True
+Get_value => False
+get_Value => False
+getValue => False
+3m => False
+m3 => True
+assert => False
+assert_exception => True '''
+
+import keyword
+import string
+
+
+#my_test = ['_', '__', '___', 'x', 'get_value', 'get value', 'get!value', 'some_super_puper_value', 'Get_value',
+#        'get_Value', 'getValue', '3m', 'm3', 'assert', 'assert_exception']
+
+
+
+my_string_punctuation = ' ' + string.punctuation.replace('_','')  # Додаємо пробіл до списку заборонених символів та виключаємо '_'
+
+#for my_variable in my_test:
+
+# Можно проверить все из my_test.. раскоментировать my_test, for и все что ниже протабулировать и убрать input
+
+my_variable=input("Ведіть ім'я змінної: ")
+
+rez = True
+
+if my_variable[0].isdigit():
+    rez = False
+elif any(c.isupper() for c in my_variable):
+    rez = False
+elif my_variable in keyword.kwlist:
+    rez = False
+#elif "__" in my_variable:
+elif len(my_variable) > 1 and my_variable.count("_")==len(my_variable):
+    rez = False
+elif any(char in my_string_punctuation for char in my_variable):
+    rez = False
+
+print(f"'{my_variable}' => {rez}")
+
