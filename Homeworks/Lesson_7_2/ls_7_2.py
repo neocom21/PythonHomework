3fa66e30de07cbbff2b36d054b0688f82270d553
neocom21 <neocom21@gmail.com>
2025-03-05 21:43:51 +0200

    lesson 7_2

---
+++ Homeworks/Lesson_7_2/ls_7_2.py
@@ -0,0 +1,14 @@
+def correct_sentence(text):
+
+  #print (text[0].upper() + text[1:].rstrip(".")+".")
+  return text[0].upper() + text[1:].rstrip(".")+"."
+
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+
+print('ОК')
+
