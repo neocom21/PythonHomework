233cbf762fad10dda820a7215512a64eabd73864
neocom21 <neocom21@gmail.com>
2025-03-11 16:54:52 +0200

    lesson 8_1_1

--- Homeworks/Lesson_8_1/ls_8_1.py
+++ Homeworks/Lesson_8_1/ls_8_1.py
@@ -1,13 +1,8 @@
+
 def add_one(some_list):
-	s=""
-	while len(some_list) > 0:
-		s+=str(some_list.pop(0))
-
-	ss=list(str(int(s) + 1))
-
-	return [int(x) for x in ss]
-
-
+    s = "".join(str(el) for el in some_list)
+    ss = list(str(int(s) + 1))
+    return [int(x) for x in ss]
 
 assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
 assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
