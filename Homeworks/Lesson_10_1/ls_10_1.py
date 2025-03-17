10c9dde3ee95913eec7856888963a3b6f8cfb996
neocom21 <neocom21@gmail.com>
2025-03-17 21:17:31 +0200

    lesson 10_1

---
+++ Homeworks/Lesson_10_1/ls_10_1.py
@@ -0,0 +1,25 @@
+
+def pow(x):
+    return x ** 2
+
+def some_gen(begin, end, func):
+    """
+     begin: перший елемент послідовності
+     end: кількість елементів у послідовності
+     func: функція, яка формує значення для послідовності
+    """
+
+    #while end!=0:
+    for _ in range(end):
+        yield begin
+        begin = func(begin)
+        #end-=1
+
+
+from inspect import isgenerator
+
+gen = some_gen(2, 4, pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
+
