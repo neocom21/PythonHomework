865b8ce42b426496f48c3002f3d8f92da17a8b28
neocom21 <neocom21@gmail.com>
2025-03-22 17:23:31 +0200

    lesson 12_1

---
+++ Homeworks/Lesson_12_1/ls_12_1.py
@@ -0,0 +1,20 @@
+
+
+import codecs
+import re
+
+def delete_html_tags(html_file, result_file='cleaned.txt'):
+
+      with codecs.open(html_file, 'r', 'utf-8') as file:
+          html = file.read()
+          matches = re.findall(r'<(title|h[1-6]|p)>(.*?)</\1>', html)
+          #print(matches)
+
+          with codecs.open(result_file, 'w')  as f:
+
+              for match in matches:
+                  #print(match[1])
+                  f.write(match[1]+"\n")
+
+
+delete_html_tags("draft.html")
