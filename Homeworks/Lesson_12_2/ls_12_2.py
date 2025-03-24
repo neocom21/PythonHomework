47798a08865ab95a1718fc84b53a98ac6374f25e
neocom21 <neocom21@gmail.com>
2025-03-24 22:23:02 +0200

    lesson 12_2

--- Homeworks/Lesson_12_2/ls_12_2.py
+++ Homeworks/Lesson_12_2/ls_12_2.py
@@ -7,7 +7,7 @@ class Item:
         self.name = name
 
     def __str__(self):
-        pass
+        return f"{self.name}, price: {self.price}" # lemon, price: 5
 
 class User:
 
@@ -17,7 +17,9 @@ class User:
         self.numberphone = numberphone
 
     def __str__(self):
-        pass
+        return f"{self.name} {self.surname}" # Ivan Ivanov
+
+
 
 class Purchase:
     def __init__(self, user):
@@ -29,33 +31,54 @@ class Purchase:
         self.products[item] = cnt
 
     def __str__(self):
-        pass
+        #User: Ivan Ivanov
+        #Items:
+        #lemon: 4 pcs.
+        #apple: 20 pcs.
+
+        tx= f"User: {self.user}\nItems: \n"
+        for item, cnt in self.products.items():
+            tx += f"{item.name}: {cnt} pcs.\n"
+
+        return tx
 
     def get_total(self):
-        pass
+        total = 0
+        for item, cnt in self.products.items():
+            total += item.price * cnt
+        return total
+
+
 
 lemon = Item('lemon', 5, "yellow", "small", )
 apple = Item('apple', 2, "red", "middle", )
+
 print(lemon)  # lemon, price: 5
 
 buyer = User("Ivan", "Ivanov", "02628162")
 print(buyer)  # Ivan Ivanov
 
+
 cart = Purchase(buyer)
 cart.add_item(lemon, 4)
 cart.add_item(apple, 20)
+
 print(cart)
+
 """
 User: Ivan Ivanov
 Items:
 lemon: 4 pcs.
 apple: 20 pcs.
 """
+
 assert isinstance(cart.user, User) is True, 'Екземпляр класу User'
 assert cart.get_total() == 60, "Всього 60"
 assert cart.get_total() == 60, 'Повинно залишатися 60!'
 cart.add_item(apple, 10)
+
 print(cart)
+
 """
 User: Ivan Ivanov
 Items:
