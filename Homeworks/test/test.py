1debefabd80be9de6d61a142882f1e2ff1fa1f96
neocom21 <neocom21@gmail.com>
2025-03-25 22:36:44 +0200

    lesson test

--- Homeworks/test/test.py
+++ Homeworks/test/test.py
@@ -1,17 +1,28 @@
-class Rectangle:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-    def calculate_area(self):
-        return self.width * self.height  # працює і для квадрата
+def capture_stdout(func):
+    return func()  # Просто викликаємо функцію та повертаємо її результат
 
 
-class Square(Rectangle):
-    def __init__(self, side):
-        super().__init__(side, side)  # Просто передаємо один параметр в обидва
+class A:
+    def show(self):
+        return "A"
 
 
-# Перевірка:
-square = Square(5)
-assert square.calculate_area() == 25
+class B(A):
+    def show(self):
+        return "B"
+
+
+class C(A):
+    def show(self):
+        return "C"
+
+
+class D(B, C):
+    def show(self):
+        return "D"
+
+
+# Перевірка
+obj_D = D()
+output_D = capture_stdout(obj_D.show)
+assert output_D == 'D'
